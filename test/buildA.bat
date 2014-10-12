cddf9d177e575d103bab075723c6ccfb77ea2981
Marius Negruțiu <marius.negrutiu@gmail.com>
2014-10-12 11:49:02 +0300

    + Building scripts prompt to launch the installer

--- test/buildA.bat
+++ test/buildA.bat
@@ -1,4 +1,8 @@
 @echo off
 set NSIS_PATH=%~dp0\..\..\NSISbin
 "%NSIS_PATH%\makensis.exe" /v4 /DANSI "%~dp0\test.nsi"
-if %ERRORLEVEL% neq 0 pause
+if %ERRORLEVEL% neq 0 pause && goto :EOF
+
+echo.
+set /p PROMPT=Execute test? (y/N) 
+if /I "%PROMPT%" == "y" "%~dp0\NSutilsA.exe"
--- test/buildW.bat
+++ test/buildW.bat
@@ -2,4 +2,8 @@
 @echo off
 set NSIS_PATH=%~dp0\..\..\NSISbin
 "%NSIS_PATH%\makensis.exe" /v4 "%~dp0\test.nsi"
-if %ERRORLEVEL% neq 0 pause
+if %ERRORLEVEL% neq 0 pause && goto :EOF
+
+echo.
+set /p PROMPT=Execute test? (y/N) 
+if /I "%PROMPT%" == "y" "%~dp0\NSutilsW.exe"
