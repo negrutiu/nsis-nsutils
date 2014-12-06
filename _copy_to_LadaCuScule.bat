c2d37dfc8a858c255cd3ec327cd5a27cc48809a9
Marius Negruțiu <marius.negrutiu@gmail.com>
2014-12-06 14:21:02 +0200

    + Fixed _copy_to_LadaCuScule.bat

--- _copy_to_LadaCuScule.bat
+++ _copy_to_LadaCuScule.bat
@@ -6,7 +6,8 @@ copy /Y "%~dp0\ReleaseA\NSutils.pdb" "%DST%\NSIS.extra\NSutils"
 copy /Y "%~dp0\NSutils.Readme.txt" "%DST%\NSIS.extra\NSutils"
 
 @if not exist "%DST%\NSISw.extra\NSutils" mkdir "%DST%\NSISw.extra\NSutils"
+copy /Y "%~dp0\ReleaseW\NSutils.dll" "%DST%\NSISw.extra\NSutils"
 copy /Y "%~dp0\ReleaseW\NSutils.pdb" "%DST%\NSISw.extra\NSutils"
-copy /Y "%~dp0\NSutils.Readme.txt" "%DST%\NSIS.extra\NSutils"
+copy /Y "%~dp0\NSutils.Readme.txt" "%DST%\NSISw.extra\NSutils"
 
 pause
