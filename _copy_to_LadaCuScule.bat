2c5eaf286bbb9a0b03e8ce78a5f16514546d44d9
Marius Negruțiu <marius.negrutiu@gmail.com>
2018-08-29 10:52:07 +0300

    Build with msys2/mingw. Build amd64 binary

--- _copy_to_LadaCuScule.bat
+++ _copy_to_LadaCuScule.bat
@@ -4,19 +4,31 @@ REM :: Note: mingw binaries are the most backward compatible (NT4+)
 set DST=%~dp0\..\LadaCuScule
 set SRCA=%~dp0\ReleaseA-mingw
 set SRCW=%~dp0\ReleaseW-mingw
+set SRC64=%~dp0\ReleaseW-mingw-amd64
 
-if not exist "%DST%\NSIS.extra\NSutils" mkdir "%DST%\NSIS.extra\NSutils"
+
+set DSTA=%DST%\NSIS.extra\NSutils
+mkdir "%DSTA%" 2> NUL
 echo on
-copy /Y "%SRCA%\NSutils.dll" "%DST%\NSIS.extra\NSutils"
-copy /Y "%SRCA%\NSutils.pdb" "%DST%\NSIS.extra\NSutils"
-copy /Y "%~dp0\NSutils.Readme.txt" "%DST%\NSIS.extra\NSutils"
+	copy "%SRCA%\NSutils.dll" "%DSTA%"
+	copy "%SRCA%\NSutils.pdb" "%DSTA%"
+	copy "%~dp0\NSutils.Readme.txt" "%DSTA%"
 @echo off
 
-if not exist "%DST%\NSISw.extra\NSutils" mkdir "%DST%\NSISw.extra\NSutils"
+set DSTW=%DST%\NSISw.extra\NSutils
+mkdir "%DSTW%" 2> NUL
 echo on
-copy /Y "%SRCW%\NSutils.dll" "%DST%\NSISw.extra\NSutils"
-copy /Y "%SRCW%\NSutils.pdb" "%DST%\NSISw.extra\NSutils"
-copy /Y "%~dp0\NSutils.Readme.txt" "%DST%\NSISw.extra\NSutils"
+	copy "%SRCW%\NSutils.dll" "%DSTW%"
+	copy "%SRCW%\NSutils.pdb" "%DSTW%"
+	copy "%~dp0\NSutils.Readme.txt" "%DSTW%"
+@echo off
+
+set DST64=%DST%\NSISamd64.extra\NSutils
+mkdir "%DST64%" 2> NUL
+echo on
+	copy "%SRC64%\NSutils.dll" "%DST64%"
+	copy "%SRC64%\NSutils.pdb" "%DST64%"
+	copy "%~dp0\NSutils.Readme.txt" "%DST64%"
 @echo off
 
 pause
