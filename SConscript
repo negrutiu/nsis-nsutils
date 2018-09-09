f9dd9826ced679f2914987de903f107b6f674ff4
Marius Negruțiu <marius.negrutiu@gmail.com>
2018-09-09 21:30:51 +0300

    Included in NSIS

---
+++ SConscript
@@ -0,0 +1,39 @@
+target = 'NSutils'
+
+files = Split("""
+	gdi.c
+	handles.c
+	main.c
+	registry.c
+	strblock.c
+	utils.c
+	verinfo.c
+	nsiswapi/pluginapi.c
+""")
+
+resources = Split("""
+	NSutils.rc
+""")
+
+libs = Split("""
+	kernel32
+	user32
+	advapi32
+	shlwapi
+	gdi32
+	ole32
+	oleaut32
+	msimg32
+	uuid
+	version
+""")
+
+examples = ''
+
+docs = Split("""
+	NSutils.Readme.txt
+""")
+
+Import('BuildPlugin')
+
+BuildPlugin(target, files, libs, examples, docs, res = resources)
---
+++ _copy_to_NSIS.bat
@@ -0,0 +1,23 @@
+@echo off
+REM | syntax: *.bat <nsis_src_root_dir>
+
+if "%~1" equ ""					echo SYNTAX: %~nx0 ^<nsis_src_root_dir^> && pause && exit /B 57
+if not exist "%~1\SConstruct"	echo ERROR: "%~1" is not an NSIS directory && pause && exit /B 57
+if not exist "%~1\Contrib"		echo ERROR: "%~1" is not an NSIS directory && pause && exit /B 57
+
+echo.
+echo ***********************
+echo ** NSutils
+echo ***********************
+
+mkdir							"%~1\Contrib\NSutils" 2> NUL
+xcopy "%~dp0\nsiswapi"			"%~1\Contrib\NSutils\nsiswapi" /EIDYF
+xcopy "%~dp0\*.h"				"%~1\Contrib\NSutils" /DYF
+xcopy "%~dp0\*.c"				"%~1\Contrib\NSutils" /DYF
+xcopy "%~dp0\*.cpp"				"%~1\Contrib\NSutils" /DYF
+xcopy "%~dp0\*.rc"				"%~1\Contrib\NSutils" /DYF
+xcopy "%~dp0\*Readme.txt"		"%~1\Contrib\NSutils" /DYF
+xcopy "%~dp0\SConscript"		"%~1\Contrib\NSutils" /DYF
+
+REM pause
+exit /B 0
