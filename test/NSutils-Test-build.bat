fb8317b2b94607f6d2601cbe9915c45d59ff9c96
Marius Negruțiu <marius.negrutiu@protonmail.com>
2024-06-02 08:07:27 +0300

    Improved the search for makensis.exe

--- test/NSutils-Test-build.bat
+++ test/NSutils-Test-build.bat
@@ -1,25 +1,17 @@
 REM :: Marius Negrutiu :: 2019/08/25
 @echo off
-SetLocal EnableDelayedExpansion
+setlocal enabledelayedexpansion
 
-if not exist "%NSIS%\makensis.exe" pushd "%~dp0..\.." && set NSIS=!CD!&& popd
-if not exist "%NSIS%\makensis.exe" set NSIS=%NSIS_INSTDIR%
+if not exist "%NSIS%\makensis.exe" pushd "%~dp0..\.." && set NSIS=!cd!&& popd
 if not exist "%NSIS%\makensis.exe" set NSIS=%PROGRAMFILES%\NSIS
 if not exist "%NSIS%\makensis.exe" set NSIS=%PROGRAMFILES(X86)%\NSIS
-if not exist "%NSIS%\makensis.exe" echo ERROR: NSIS not found ^(Tip: NSIS_INSTDIR can be defined to point to NSIS binaries^) && pause && exit /B 2
+if not exist "%NSIS%\makensis.exe" for /f "delims=*" %%f in ('where makensis.exe 2^> nul') do pushd "%%~dpf" && set NSIS=!cd!&& popd
+if not exist "%NSIS%\makensis.exe" echo ERROR: NSIS not found&& pause && exit /b 2
 
 echo ********************************************************************************
 echo %NSIS%\makensis.exe
 echo ********************************************************************************
 
-Title Build: amd64-unicode
-"%NSIS%\makensis.exe" /V4 /DAMD64 "%~dp0\NSutils-Test.nsi"
-if %errorlevel% neq 0 pause && exit /B %errorlevel%
-
-Title Build: x86-ansi
-"%NSIS%\makensis.exe" /V4 /DANSI "%~dp0\NSutils-Test.nsi"
-if %errorlevel% neq 0 pause && exit /B %errorlevel%
-
-Title Build: x86-unicode
-"%NSIS%\makensis.exe" /V4 "%~dp0\NSutils-Test.nsi"
-if %errorlevel% neq 0 pause && exit /B %errorlevel%
+title Build: amd64-unicode&& "%NSIS%\makensis.exe" /V4 /DAMD64 "%~dp0\NSutils-Test.nsi" || pause && exit /b !errorlevel!
+title Build: x86-ansi&&      "%NSIS%\makensis.exe" /V4 /DANSI  "%~dp0\NSutils-Test.nsi" || pause && exit /b !errorlevel!
+title Build: x86-unicode&&   "%NSIS%\makensis.exe" /V4         "%~dp0\NSutils-Test.nsi" || pause && exit /b !errorlevel!
